b821296e81d4423c17fb3d8b2c5c40ba6cdc96fd
hess3077 <hess.website@gmail.com>
2019-09-06 20:49:48 +0200

    listing movies

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+// start_session
+
+try {
+    session_start();
+}
+catch(\Exception $e) {}
+
+// Include services
+
+include("Services/Search.php");
+
+$search = new Search();
+$movies = $search->getMovies();
+
+var_dump($movies, 'list movies');
+
+
+//var_dump($_SESSION['movies'], 'SESSION');
